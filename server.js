aded6bc1db1b9e798c8b163ffa9e37bbb88b6d23
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-27 11:02:17 -0700

    Update server.js with final updates, but it's still not working correctly

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ var schema = buildSchema(`
     age: Int
     shark: String
   }
+  type Mutation {
+    updateUser(id: Int!, name: String!, age: String): Person
+  }
 `);
 
 // Sample users
@@ -54,7 +57,7 @@ var users = [
 var getUser = function(args) {
   var userID = args.id;
   return users.filter(user => user.id == userID)[0];
-};
+}
 
 // Return a list of users (takes an optional shark parameter)
 var retrieveUsers = function(args) {
@@ -64,12 +67,25 @@ var retrieveUsers = function(args) {
   } else {
     return users;
   }
-};
+}
+
+// Update a user and return new user details
+var updateUser = function({id, name, age}) {
+  users.map(user => {
+    if (user.id === id) {
+      user.name = name;
+      user.age = age;
+      return user;
+    }
+  });
+  return users.filter(user => user.id === id)[0];
+}
 
 // Root resolver
 var root = {
-  user: getUser,  // Resolver function to return user with specific id
-  users: retrieveUsers
+  user: getUser,
+  users: retrieveUsers,
+  updateUser: updateUser  // Include mutation function in root resolver
 };
 
 // Create an express server and a GraphQL endpoint
